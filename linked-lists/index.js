96d18372398c19368a97f150c8a249af76c1979c
Abigail White <abigailwhite213@gmail.com>
2017-06-28 22:21:03 -0600

    remove SLL

--- linked-lists/index.js
+++ linked-lists/index.js
@@ -4,10 +4,15 @@ const Node = require('./list-node');
 const SLL = require('./linked-list');
 
 let sll = new SLL();
-sll.head = new Node(1);
-sll.head.next = new Node(2);
-sll.head.next.next = new Node(3);
-sll.head.next.next.next = new Node(4);
+sll.head = new Node(5);
+sll.head.next = new Node(10);
+sll.head.next.next = new Node(12);
+sll.head.next.next.next = new Node(2);
 
-let reversed = sll.reverse(sll);
-console.log(reversed);
+console.log(sll);
+
+let remove = sll.remove(sll, 10);
+console.log(remove);
+
+let reverse = sll.reverse(sll);
+console.log(reverse);
